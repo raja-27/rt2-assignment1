f783f7de563d6c69ef873ec1ec5635b12a28d0a1
raja-27 <81862489+raja-27@users.noreply.github.com>
2022-07-06 22:46:57 +0200

    Add files via upload

---
+++ html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "cdfgmnprsu",
+  1: "gpsu",
+  2: "cdfgmnru"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Files",
+  2: "Functions"
+};
+
